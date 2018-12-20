5b72d13b1229313c5598a5b6af30cc284a81490b
tsion <tsionac@post.bgu.ac.il>
2018-12-20 16:26:34 +0200

    for check

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,27 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import * as esgen from 'escodegen';
+import {parseCode, substituteCode,evaluation} from './code-analyzer';
 
 $(document).ready(function () {
-    $('#codeSubmissionButton').click(() => {
+    $('#parse').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
     });
+    let Subcode;
+    $('#codeSubmissionButton').click(() => {
+        let codeToParse = $('#codePlaceholder').val();
+        Subcode = substituteCode(codeToParse);
+        $('#parsedCode').val(Subcode);
+    });
+    $('#evaluateButton').click(() => {
+        let input = JSON.parse(($('#input').val()));
+        let obj = parseCode(Subcode);
+        let evaluate =evaluation(obj, input);
+        document.getElementById('colorEval').innerHTML = esgen.generate(evaluate,{verbatim: 'color'});
+    });
 });
+
+
+
+//var json = JSON.stringify(parsedCode);
